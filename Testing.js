941578eebf54b3f657b0dcdd5ce62314eafc3728
Md Jalal Molla <98682903+Jalal-Molla@users.noreply.github.com>
2024-11-28 21:32:12 +0600

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -1,72 +1,64 @@
-const person={
-  PersonalInformation:{
-    Name:"Md Jalal Molla",
-    NickName:"Md Easmile Molla",
-    Age:"28+",
-    Education:"BScinCSE",
-    Learning:"React",
-    Hope:"TobeSoftwareEngineer",
-    Preferedlang:"C++/Java/Python/JavaScript",
-    Framework:"JUIC/Spring/Django/React",
-    Database:"SQL/MongoDB",
-    Otherintesnsion:"DataScience/MachineLearning"
-  },
-  theme: {
-    backgroundColor: 'yellow',
-    padding:"5px",
-    margin:"500px",
-    width:"500px",
-    color: "black",
-    border:"5px solid black"
-},
-jalal:{
-    backgroundColor:'blue',
-    padding:"10px",
-    //margin:"500px",
-    color:"black",
-    border:"1.5px solid black"
+const person = {
+    PersonalInformation: {
+      Name: "Md Jalal Molla",
+      NickName: "Md Easmile Molla",
+      Age: "28+",
+      Education: "BSc in CSE",
+      Learning: "React",
+      Hope: "To be Software Engineer",
+      PreferredLang: "C++/Java/Python/JavaScript",
+      Framework: "JUIC/Spring/Django/React",
+      Database: "SQL/MongoDB",
+      OtherInterest: "Data Science/Machine Learning",
+    },
+    theme: {
+      backgroundColor: "yellow",
+      padding: "5px",
+      margin: "20px auto",
+      width: "500px",
+      color: "black",
+      border: "5px solid black",
+    },
+    jalal: {
+      backgroundColor: "blue",
+      padding: "10px",
+      color: "white",
+      border: "1.5px solid black",
+      margin: "10px 0",
+    },
+    image: {
+      width: "150px", // ইমেজের প্রস্থ
+      height: "150px", // ইমেজের উচ্চতা
+      borderRadius: "50%", // বৃত্তাকার ইমেজ
+      display: "block",
+      border: "3px solid black",
+      margin: "10px auto", // কেন্দ্রীয় করার জন্য
+    },
+  };
 
-},
-image: {
-    width: "150px", // ইমেজের প্রস্থ
-    height: "150px", // ইমেজের উচ্চতা
-    borderRadius: "50%", // বৃত্তাকার ইমেজ
-    display: "block",
-    border:"3px solid black", // কেন্দ্রীয় করার জন্য
-    margin: "10px auto" // অটো মার্জিন
-}
+  const today = new Date();
 
-}
+  function TodayFormat(date) {
+    return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+  }
 
-const today=new Date();
-function TodayFormat(date){
-  return new Intl.DatetimeFormat(
-    'en-US',
-    {weekday:'long'}
-
-  ).format(date);
-
-}
-
-export default function MyApp(){
-  return(
-    <div>
-      <div style={person.theme}>
-        <h1>Today is: {TodayFormat(today)}</h1>
-        <h2>Here is my inforamtion have a look!</h2>
-        <img src="easmile.png"
-        style={person.image}
-        alt="Profile"
-        />
-        {Object.entries(person.PersonalInformation).map(([key,value])=>(
-          <div style={person.jalal} key={key}>
-            <strong>{key}:</strong> {value}
-
-          </div>
-        ))}
-
+  export default function MyApp() {
+    return (
+      <div>
+        <div style={person.theme}>
+          <h1>Today is: {TodayFormat(today)}</h1>
+          <h2>Here is my information, have a look!</h2>
+          <img
+            src="easmile.png"
+            style={person.image}
+            alt="Profile"
+          />
+          {Object.entries(person.PersonalInformation).map(([key, value]) => (
+            <div style={person.jalal} key={key}>
+              <strong>{key}:</strong> {value}
+            </div>
+          ))}
+        </div>
       </div>
-
-    </div>
-  );
-}
+    );
+  }
